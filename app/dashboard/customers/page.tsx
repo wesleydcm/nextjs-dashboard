d75602896dc7e5850ccf2319d396d22f16a4baeb
Wesley Matos <wesleycostamatos@gmail.com>
2024-04-04 18:43:52 -0300

    chore: add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,14 @@
+import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
-  return <p>Customers Page</p>;
+  return (
+    <h1 className={`${lusitana.className} text-2xl`}>
+      Customers... page under construction
+    </h1>
+  );
 }
